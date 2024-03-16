886922f72b77166300925e3f60f30246d6422c66
Manishdudi21 <160268803+Manishdudi21@users.noreply.github.com>
2024-03-16 22:10:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ from dotenv import load_dotenv
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 load_dotenv(override=True)
 
-API_ID = int(os.getenv("TG_API_ID", "5120"))
-API_HASH = os.getenv("TG_API_HASH", "1fda88a5d1de478bce198e")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "token")
+API_ID = int(os.getenv("TG_API_ID", "23449041"))
+API_HASH = os.getenv("TG_API_HASH", "22263bf0807d45f0fc9bac99471fec1b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6834248596:AAFQt-3sS27YNKMZix7yFKOHkalRyWNQJhs")
 MONGODB_URL = os.getenv("MONGODB_URL", "mongouri")
-BOT_USERNAME = None
+BOT_USERNAME = @Allliswell01bot
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 mongo_client = MongoClient(MONGODB_URL, server_api=ServerApi('1'))
 download_folder = 'files'
